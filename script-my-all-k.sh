1d1e6a4770b792c28c819ff79607ff26b0b03cbe
Ashish Kumar <ashishkr23438@gmail.com>
2021-04-10 11:20:08 +0530

    Change my approach to fixed k

---
+++ script-my-all-k.sh
@@ -0,0 +1,60 @@
+
+
+nodes=5
+algo='my-approach.py'
+startport=8001
+endport=$((startport-1+nodes))
+
+
+algo_without_ext="${algo%.*}"
+
+if [ ! -d "results" ]; then
+	mkdir "results"
+fi
+
+if [ ! -d "results/log-${algo_without_ext}" ]; then
+	mkdir "results/log-${algo_without_ext}"
+fi
+
+if [ ! -f "results/log-${algo_without_ext}/result.csv" ]; then
+	echo "Algo,Nodes,Network Filename,History Length,Sent Messages,Received Messages" > "results/log-${algo_without_ext}/result.csv"
+fi
+
+for (( history_count=2; history_count<=25; history_count++ ));
+do
+	rm -rf results/log-${algo_without_ext}/${nodes}/*
+	for file in data/$nodes/*;
+	do
+		# Start the server
+		filename=${file##*/}
+		./start-servers-my.sh 1 $nodes $algo $filename $history_count
+
+		# Start the election
+		sleep 10s
+		python3 start_election.py --startport=$startport --endport=$endport
+
+		# Check if every node receives the leader ID
+		sleep 1s
+		filename_without_ext="${filename%.*}"
+		while true;
+		do
+			if [[ $(cat "results/log-${algo_without_ext}/${nodes}/${filename_without_ext}-count.txt") -eq $nodes ]];
+			then
+				break
+			fi
+			sleep 1s
+		done
+
+		# Save the result
+		sleep 1s
+		log_file="results/log-${algo_without_ext}/${nodes}/${filename_without_ext}.txt"
+		sent=$(cat $log_file | grep 'Sent' | wc -l)
+		received=$(cat $log_file | grep 'Received' | wc -l)
+		echo "${algo_without_ext},${nodes},${filename_without_ext},${history_count},${sent},${received}" >> "results/log-${algo_without_ext}/result.csv"
+
+
+		sleep 1s
+		./stop-servers.sh $startport $endport
+	done
+
+done
--- script-my.sh
+++ script-my.sh
@@ -20,41 +20,37 @@ if [ ! -f "results/log-${algo_without_ext}/result.csv" ]; then
 	echo "Algo,Nodes,Network Filename,History Length,Sent Messages,Received Messages" > "results/log-${algo_without_ext}/result.csv"
 fi
 
-for (( history_count=2; history_count<=25; history_count++ ));
+history_count=5
+rm -rf results/log-${algo_without_ext}/${nodes}/*
+for file in data/$nodes/*;
 do
-	rm -rf results/log-${algo_without_ext}/${nodes}/*
-	for file in data/$nodes/*;
+	# Start the server
+	filename=${file##*/}
+	./start-servers-my.sh 1 $nodes $algo $filename $history_count
+
+	# Start the election
+	sleep 10s
+	python3 start_election.py --startport=$startport --endport=$endport
+
+	# Check if every node receives the leader ID
+	sleep 1s
+	filename_without_ext="${filename%.*}"
+	while true;
 	do
-		# Start the server
-		filename=${file##*/}
-		./start-servers-my.sh 1 $nodes $algo $filename $history_count
-
-		# Start the election
-		sleep 10s
-		python3 start_election.py --startport=$startport --endport=$endport
-
-		# Check if every node receives the leader ID
+	if [[ $(cat "results/log-${algo_without_ext}/${nodes}/${filename_without_ext}-count.txt") -eq $nodes ]];
+		then
+			break
+		fi
 		sleep 1s
-		filename_without_ext="${filename%.*}"
-		while true;
-		do
-			if [[ $(cat "results/log-${algo_without_ext}/${nodes}/${filename_without_ext}-count.txt") -eq $nodes ]];
-			then
-				break
-			fi
-			sleep 1s
-		done
-
-		# Save the result
-		sleep 1s
-		log_file="results/log-${algo_without_ext}/${nodes}/${filename_without_ext}.txt"
-		sent=$(cat $log_file | grep 'Sent' | wc -l)
-		received=$(cat $log_file | grep 'Received' | wc -l)
-		echo "${algo_without_ext},${nodes},${filename_without_ext},${history_count},${sent},${received}" >> "results/log-${algo_without_ext}/result.csv"
-
-
-		sleep 1s
-		./stop-servers.sh $startport $endport
 	done
 
+	# Save the result
+	sleep 1s
+	log_file="results/log-${algo_without_ext}/${nodes}/${filename_without_ext}.txt"
+	sent=$(cat $log_file | grep 'Sent' | wc -l)
+	received=$(cat $log_file | grep 'Received' | wc -l)
+	echo "${algo_without_ext},${nodes},${filename_without_ext},${history_count},${sent},${received}" >> "results/log-${algo_without_ext}/result.csv"
+
+	sleep 1s
+	./stop-servers.sh $startport $endport
 done
